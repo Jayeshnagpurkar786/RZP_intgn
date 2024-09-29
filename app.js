6d7b90f603de87e839576ccb51fb7a88590cbe19
Jayesh Nagpurkar <jayeshnagpurkar2@gmail.com>
2024-09-29 15:49:31 +0530

    Favicon updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const favicon = require('serve-favicon');
+const path = require('path');
 const routes = require('./routes/routes');
 
 // Load environment variables
@@ -24,9 +25,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // Adjust this path as necessary
+
+// Respond to favicon requests if not provided
+app.get('/favicon.ico', (req, res) => res.status(204)); // Respond with no content for favicon
+
 // Routes
-const route = require("./routes/routes"); // Ensure your routes are defined correctly in routes.js
-app.use("/api", route); // This will prefix all routes in routes.js with /api
+app.use("/api", routes); // This will prefix all routes in routes.js with /api
 
 // Health check route for checking if API is up
 app.get("/api", (req, res) => {
